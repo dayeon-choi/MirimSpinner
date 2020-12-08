8ec9ffcbbfee59f8c6c39a4a22701e4495e8d606
dayeon <dayeon2399@naver.com>
2020-12-08 19:19:20 +0900

    dayeon's commit - spinner 첫번째

--- app/src/main/java/org/techtown/mirimspinner/MainActivity.java
+++ app/src/main/java/org/techtown/mirimspinner/MainActivity.java
@@ -3,12 +3,22 @@ package org.techtown.mirimspinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
+    String[] titleRes={"기생충","설국열차","택시운전사","군함도","반도","레미제라블","라라랜드","메이즈러너","부산행","살아있다"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner1=findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,titleRes);
+        spinner1.setAdapter(adapter);
+
     }
 }
